fb7958f704c8c9cdae2743f1eed747605336bb7c
Devasena k <88845916+senaprojects@users.noreply.github.com>
2023-08-13 21:32:13 +0530

    backend_socialmedia

---
+++ index.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const multer = require("multer");
+const userRoute = require("./routes/users");
+const authRoute = require("./routes/auth");
+const postRoute = require("./routes/posts");
+const router = express.Router();
+const path = require("path");
+const cors = require("cors");
+dotenv.config();
+const PORT = process.env.PORT;
+const CONNECTION_URL = process.env.MONGO_URL;
+
+app.use("/images", express.static(path.join(__dirname, "public/images")));
+app.get("/", (req, res) => res.send("Hello World"));
+//middleware
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+app.use(cors());
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "public/images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, req.body.name);
+  },
+});
+
+const upload = multer({ storage: storage });
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  try {
+    return res.status(200).json("File uploded successfully");
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/posts", postRoute);
+mongoose
+  .connect(CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() =>
+    app.listen(PORT, () => {
+      console.log(`Backend server is running!${PORT}`);
+    })
+  )
+  .catch((err) => console.log(err));
